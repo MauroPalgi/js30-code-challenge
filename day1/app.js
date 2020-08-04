45ed264c66639ff0c8716808346f9652c37b69df
Mauro Palgi <maurolink@gmail.com>
2020-08-04 10:34:44 -0300

    day1 - fix responsive

--- day1/app.js
+++ day1/app.js
@@ -1,17 +1,19 @@
 window.addEventListener("keydown", playSound);
 
-function playSound (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  
-    if (!audio) return;
-  
-    // AUDIO
-    audio.currentTime = 0; // regresa al comienzo
-    audio.play(); // regresa al comienzoa
-  
-    // STYLE
-    key.classList.add("playing");
+function playSound(e) {
+  console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  console.log(audio);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+
+  // AUDIO
+  audio.currentTime = 0; // regresa al comienzo
+  audio.play(); // regresa al comienzoa
+
+  // STYLE
+  key.classList.add("playing");
 }
 
 // REMOVE TRANSITION
@@ -19,7 +21,6 @@ const keys = document.querySelectorAll(".key");
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
-  console.log(this);
   this.classList.remove("playing");
 }
 
